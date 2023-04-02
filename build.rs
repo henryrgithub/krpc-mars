ee1918db7ff6698bd4814f5fb5a4aac4c8f455c5
henryrgithub <68022539+henryrgithub@users.noreply.github.com>
2023-04-02 12:21:15 -0700

    undo test

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
 fn main() {
     println!("cargo:rerun-if-changed=protos/krpc.proto");
     println!("cargo:rerun-if-changed=src/krpc.rs");
-    /*protoc_rust::Codegen::new()
+    protoc_rust::Codegen::new()
         .out_dir("src/")
         .inputs(&["protos/krpc.proto"])
         .run()
         .expect("protoc");
-    */
+    
     //println!("mars build.rs doing things");
 }
