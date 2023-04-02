faa7ff12adba1d89c5c6941e0fe460a7d1ce9e3e
henryrgithub <68022539+henryrgithub@users.noreply.github.com>
2023-04-02 11:35:48 -0700

    rebuild to see if that fixes perennial rebuild

--- build.rs
+++ build.rs
@@ -6,5 +6,5 @@ fn main() {
         .inputs(&["protos/krpc.proto"])
         .run()
         .expect("protoc");
-    println!("mars build.rs doing things");
+    //println!("mars build.rs doing things");
 }
