bb48423476154edc5c48205fba4a23cda91d5ed5
henryrgithub <68022539+henryrgithub@users.noreply.github.com>
2023-04-02 14:22:23 -0700

    another test
    shouldn't it not rerun based on generate dealio?

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("cargo:rerun-if-changed=protos/krpc.proto");
-    println!("cargo:rerun-if-changed=src/krpc.rs");
+    //println!("cargo:rerun-if-changed=src/krpc.rs");
     protoc_rust::Codegen::new()
         .out_dir("src/")
         .inputs(&["protos/krpc.proto"])
